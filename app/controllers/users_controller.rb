89943c0326c42eb7bfa08f0fc6103515452b0b58
user <user@usernoMacBook-Pro.local>
2019-06-01 17:50:45 +0900

    user登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,22 @@ def index
 end
 
 def new
-  @user = User.new(user_params)
+  @user = User.new
 end
 
 def create
+  @user = User.new(user_params)
+  if @user.save
+  else
+    render 'new'
+  end
 end
 
 
 
 private
-params.require(:user).permit(:name,:email,:password_digest)
 
-
-
-
+def user_params
+params.require(:user).permit(:name,:email,:password,:password_confirmation)
+end
 end

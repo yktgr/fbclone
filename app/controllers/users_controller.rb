e9eec0dc6fc172f54b8fb888e82b794208e347f4
user <user@usernoMacBook-Pro.local>
2019-06-02 16:13:11 +0900

    login機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,15 @@ end
 def create
   @user = User.new(user_params)
   if @user.save
+    redirect_to user_path(@user.id)
   else
     render 'new'
   end
 end
 
+def show
+  @user = User.find(params[:id])
+end
 
 
 private

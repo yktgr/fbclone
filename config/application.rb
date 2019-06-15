e1f07ddd9ff7fd8298971a2273b3db5cccc35aba
user <user@usernoMacBook-Pro.local>
2019-06-15 19:51:05 +0900

    autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Fb
     config.load_defaults 5.1
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
